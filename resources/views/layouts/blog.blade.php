cc163a3cdeabf6a434ebca1e014ff0ffb4c12c46
touros91 <andreageraci91@gmail.com>
2021-11-16 16:29:31 +0100

    Refactoring - Esercizio completo

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -46,12 +46,12 @@
                 @if (Route::has('login'))
                     <div class="top-right links">
                         @auth
-                            <a href="{{ route('admin.home') }}">Home</a>
+                            <a style="margin-left: 20px;" href="{{ route('admin.home') }}">Home</a>
                         @else
-                            <a href="{{ route('login') }}">Login</a>
+                            <a style="margin-left: 20px;" href="{{ route('login') }}">Login</a>
 
                             @if (Route::has('register'))
-                                <a href="{{ route('register') }}">Register</a>
+                                <a style="margin-left: 20px;" href="{{ route('register') }}">Register</a>
                             @endif
                         @endauth
                     </div>
